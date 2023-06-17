10313196997143fdf34bc6c2aecd101eebeea6ab
Toh_Wee_Alex <tohtzehsuan@tohtzeh.page>
2023-06-17 22:25:47 +0800

    updated NavBar

---
+++ docs/Elements/MouseOverMtd.js
@@ -0,0 +1,15 @@
+function mOverHTML() {
+    document.getElementById("HiddenNavHTML").style.visibility = "visible";
+}
+
+function mOutHTML() {
+    document.getElementById("HiddenNavHTML").style.visibility = "hidden";
+}
+
+function mOverCSS() {
+    document.getElementById("HiddenNavCSS").style.visibility = "visible";
+}
+
+function mOutCSS() {
+    document.getElementById("HiddenNavCSS").style.visibility = "hidden";
+}
