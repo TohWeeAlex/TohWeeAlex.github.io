34e64f5e0aeaefb12f222dd94cde1a7e2078ae99
Toh_Wee_Alex <tohtzehsuan@tohtzeh.page>
2023-06-21 20:29:39 +0800

    Art and NavBar updated
    3D model and illustration page updated

--- docs/Elements/MouseOverMtd.js
+++ docs/Elements/MouseOverMtd.js
@@ -1,3 +1,4 @@
+// HTML
 function mOverHTML() {
     document.getElementById("HiddenNavHTML").style.visibility = "visible";
 }
@@ -6,6 +7,7 @@ function mOutHTML() {
     document.getElementById("HiddenNavHTML").style.visibility = "hidden";
 }
 
+// CSS
 function mOverCSS() {
     document.getElementById("HiddenNavCSS").style.visibility = "visible";
 }
@@ -13,3 +15,13 @@ function mOverCSS() {
 function mOutCSS() {
     document.getElementById("HiddenNavCSS").style.visibility = "hidden";
 }
+
+// Miscellaneous
+function mOverMics() {
+    document.getElementById("HiddenNavMics").style.visibility = "visible";
+}
+
+function mOutMics() {
+    document.getElementById("HiddenNavMics").style.visibility = "hidden";
+}
+
