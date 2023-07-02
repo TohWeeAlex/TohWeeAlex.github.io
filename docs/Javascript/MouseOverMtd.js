357ba87c45ca614ab7f76119272d835ca1af7c51
Toh_Wee_Alex <tohtzehsuan@tohtzeh.page>
2023-07-02 18:27:13 +0800

    bootstrap integrated to website,
    bootstrap 5 is used, general visual overhaul

---
+++ docs/Javascript/MouseOverMtd.js
@@ -0,0 +1,36 @@
+// HTML
+function mOverHTML() {
+    document.getElementById("HiddenNavHTML").style.visibility = "visible";
+}
+
+function mOutHTML() {
+    document.getElementById("HiddenNavHTML").style.visibility = "hidden";
+}
+
+// CSS
+function mOverCSS() {
+    document.getElementById("HiddenNavCSS").style.visibility = "visible";
+}
+
+function mOutCSS() {
+    document.getElementById("HiddenNavCSS").style.visibility = "hidden";
+}
+
+// Miscellaneous
+function mOverMics() {
+    document.getElementById("HiddenNavMics").style.visibility = "visible";
+}
+
+function mOutMics() {
+    document.getElementById("HiddenNavMics").style.visibility = "hidden";
+}
+
+// Java
+function mOverJava() {
+    document.getElementById("HiddenNavJava").style.visibility = "visible";
+}
+
+function mOutJava() {
+    document.getElementById("HiddenNavJava").style.visibility = "hidden";
+}
+
