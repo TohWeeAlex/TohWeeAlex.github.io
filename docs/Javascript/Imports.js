1f24a905b2424ac248df9af24455072f649e61e6
Toh_Wee_Alex <tohtzehsuan@tohtzeh.page>
2023-07-22 13:04:36 +0800

    view persistence, script loading
    added view mode session persistence and fix CDN and external script loading sequence.

--- docs/Javascript/Imports.js
+++ docs/Javascript/Imports.js
@@ -22,13 +22,21 @@ async function getNavBar(filePath) {
 }
 
 
-getNavBar("Elements/nav.htm");
-loadScript("https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js");
 loadScript("https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js");
+loadScript("https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js");
 
-
-loadScript("Javascript/MouseOverMtd.js");
-loadScript("Javascript/LightDarkMode.js");
 
 loadCSS("https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css");
 loadCSS("Style/styles.css");
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  // Your JavaScript code here
+  // This code will execute once the DOM is ready
+  getNavBar("Elements/nav.htm");
+  loadScript("Javascript/MouseOverMtd.js");
+  loadScript("Javascript/LightDarkMode.js");
+});
+
+
+
--- docs/Javascript/LightDarkMode.js
+++ docs/Javascript/LightDarkMode.js
@@ -1,27 +1,72 @@
+function darkMode() {
+    // Add Dark classes
+    $("nav.navbar").addClass("dark-mode");
+    $("a.nav-link").addClass("text-light");
+    $("body").addClass("dark-mode");
+    // Remove light class
+    $("nav.navbar").removeClass("light-mode");
+    $("a.nav-link").removeClass("text-dark");
+    $("body").removeClass("light-mode");
+    // Change session item value
+    sessionStorage.setItem("viewMode", "dark");
+}
+
+function lightMode() {
+    // Add Light classes
+    $("nav.navbar").addClass("light-mode");
+    $("a.nav-link").addClass("text-dark");
+    $("body").addClass("light-mode");
+    // Remove Dark classes
+    $("nav.navbar").removeClass("dark-mode");
+    $("a.nav-link").removeClass("text-light");
+    $("body").removeClass("dark-mode");
+    // Change session item value
+    sessionStorage.setItem("viewMode", "light");
+}
+
 function viewChange() {
-    // Dark mode
-    $("nav.navbar").toggleClass("dark-mode");
-    $("a.nav-link").toggleClass("text-light");
-    $("body").toggleClass("dark-mode");
-
-    // Light mode
-    $("nav.navbar").toggleClass("light-mode");
-    $("a.nav-link").toggleClass("text-dark");
-    $("body").toggleClass("light-mode");
-
     // View mode indicator
-    if (($("#mode-text").text()) == "Light Mode") {
+    if (($("#mode-text").text()) === "Light Mode") {
         $("#mode-text").text("Dark Mode");
     }
     else {
         $("#mode-text").text("Light Mode");
     }
+
+    switch (sessionStorage.getItem("viewMode")) {
+        case null:
+            darkMode();
+            console.log("click: dark");
+            break;
+        case "light":
+            darkMode();
+            console.log("click: dark");
+            break;
+        case "dark":
+            lightMode();
+            console.log("click: light");
+            break;
+    }
 }
 
 function initialMode() {
-    $("nav.navbar").toggleClass("light-mode");
-    $("a.nav-link").toggleClass("text-dark");
-    $("body").toggleClass("light-mode");
+    switch (sessionStorage.getItem("viewMode")) {
+        case null:
+            lightMode();
+            console.log("initial:" + sessionStorage.getItem("viewMode"));
+            break;
+        case "light":
+            lightMode();
+            console.log("initial:" + sessionStorage.getItem("viewMode"));
+            break;
+        case "dark":
+            darkMode();
+            document.getElementById("view-mode").setAttribute("checked", "");
+            $("#mode-text").text("Dark Mode");
+            console.log("initial:" + sessionStorage.getItem("viewMode"));
+            break;
+    }
 }
 
 initialMode();
+console.log(sessionStorage.getItem("viewMode"));
