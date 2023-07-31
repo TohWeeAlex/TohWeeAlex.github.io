1cde72c1634387a3622bac7bd0f361bb3bdc9eae
Toh_Wee_Alex <tohtzehsuan@tohtzeh.page>
2023-07-31 23:38:29 +0800

    missing files

--- docs/Javascript/RouletteWheel.js
+++ docs/Javascript/RouletteWheel.js
@@ -6,6 +6,7 @@ $(document).ready(function () {
   initWheel();
 
   $("button").on("click", function () {
+    document.getElementById( 'spin-wheel' ).disabled = true;
     spinWheel();
   });
 });
@@ -16,15 +17,15 @@ async function initWheel() {
   let data = await fetch("https://valorant-api.com/v1/agents?isPlayableCharacter=true");
   // Parse the response to JSON
   let agents = await data.json();
+  console.log(agents.data);
   // Get all Agent names
   for (let i = 0; i < agents.data.length; i++) {
     //console.log("from apiFetch: " + i + " " + agents.data[i].displayName + " class: " + agents.data[i].role.displayName );
     //console.log(agents.data[i].displayIcon);
-    agentsList.push([agents.data[i].displayName, agents.data[i].role.displayName, agents.data[i].displayIcon]);
+    agentsList.push([agents.data[i].displayName, agents.data[i].role.displayName, agents.data[i].displayIcon, agents.data[i].backgroundGradientColors]);
   }
 
   console.log(agentsList);
-
   var $wheel = $(".roulette-wrapper .wheel"),
     row = "";
 
@@ -54,12 +55,12 @@ async function initWheel() {
   for (let i = 0; i < agentsList.length; i++) {
     //console.log(agentsList[i][2]);
     //row += "  <div class='card border' style='background-image:" + agentsList[i][2] + ";'></div>";
-    row += "  <div class='card border'><img src='" + agentsList[i][2] + "' class='card-img object-fit-cover'></div>";
+    row += "  <div class='card border border-3'style='background-image:linear-gradient(#" + agentsList[i][3][0] + ", #" + agentsList[i][3][1] + ", #" + agentsList[i][3][2] + ", #" + agentsList[i][3][3] + ");'><img src='" + agentsList[i][2] + "' class='card-img object-fit-cover'></div>";
     //console.log("  <div class='card border'><img class='agent-icon' src='" + agentsList[i][2] + "></div>")
   }
   row += "</div>";
 
-  for (var x = 0; x < 29; x++) {
+  for (var x = 0; x < 45; x++) {
     $wheel.append(row);
   }
 }
@@ -68,12 +69,12 @@ function spinWheel() {
   var $wheel = $(".roulette-wrapper .wheel"),
     order = [0, 18, 11, 5, 21, 10, 6, 9, 20, 16, 7, 8, 1, 14, 17, 2, 13, 3, 12, 4, 15, 19],
     //position = order.indexOf(roll);
-    position = Math.floor(Math.random() * order.length);
+    position = order.indexOf(Math.floor(Math.random() * 22));
 
   //determine position where to land
   var rows = 12,
-    card = 75 + 3 * 2,
-    landingPosition = rows * 21 * card + position * card;
+    card = 75 + 3.5 * 2,
+    landingPosition = rows * 22 * card + position * card;
 
   var randomize = Math.floor(Math.random() * 75) - 75 / 2;
 
@@ -88,35 +89,18 @@ function spinWheel() {
     "transition-timing-function":
       "cubic-bezier(0," + object.x + "," + object.y + ",1)",
     "transition-duration": "6s",
-    transform: "translate3d(-" + (landingPosition) + "px, 0px, 0px)"
+    transform: "translate3d(-" + landingPosition + "px, 0px, 0px)"
   });
 
-  
   setTimeout(function () {
     $wheel.css({
       "transition-timing-function": "",
       "transition-duration": ""
     });
-    
-    //var resetTo = -(position + randomize);
-    //$wheel.css("transform", "translate3d(" + resetTo + "px, 0px, 0px)");
-  },6 * 1000);
 
-}
+    document.getElementById( 'spin-wheel' ).disabled = false;
+    var resetTo = -(position * card + randomize);
+    $wheel.css("transform", "translate3d(" + resetTo + "px, 0px, 0px)");
+  }, 6 * 1000);
 
-async function apiFetch() {
-  var agentsList = [];
-  // Get request to valorant API
-  let data = await fetch("https://valorant-api.com/v1/agents?isPlayableCharacter=true");
-  // Parse the response to JSON
-  let agents = await data.json();
-  console.log(agents);
-  // Get all Agent names
-  for (let i = 0; i < agents.data.length; i++) {
-    //console.log("from apiFetch: " + i + " " + agents.data[i].displayName + " class: " + agents.data[i].role.displayName );
-    //console.log(agents.data[i].displayIcon);
-    agentsList.push([agents.data[i].displayName, agents.data[i].role.displayName, agents.data[i].displayIcon]);
-  }
-  //console.log(agentsList);
-  return agentsList;
 }
