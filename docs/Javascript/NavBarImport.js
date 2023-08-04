f2fce2deebc434d3b2c5c7eca0ee54d76881f7db
Toh_Wee_Alex <tohtzehsuan@tohtzeh.page>
2023-08-04 14:23:40 +0800

    removed more unused elements
    javascripts and css files that were not being used, have been removed

--- docs/Javascript/NavBarImport.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('DOMContentLoaded', function() {
-    fetch('Elements/nav.htm')
-    .then(response => response.text())
-    .then(html => {
-        document.getElementById('NavBar').innerHTML = html;
-    });
-});
